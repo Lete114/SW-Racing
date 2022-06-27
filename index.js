3f2b550461f4674920140046b3b9fe14057930d7
Lete114 <me@imlete.cn>
2022-06-27 16:35:47 +0800

    fix: 在一些情况下导致无限刷新的bug

--- index.js
+++ index.js
@@ -21,9 +21,10 @@ hexo.extend.filter.register('after_generate', function () {
           if (!localStorage.getItem('installSW')) {
             localStorage.setItem('installSW', true)
             // 这里就不用清理setInterval了，因为页面刷新后就没有了
-            setInterval(() => {
+            const timer = setInterval(() => {
               // 判断sw安装后，是否处于激活状态，激活后刷新页面
               if (result && result.active && result.active.state === 'activated') {
+                clearInterval(timer)
                 location.reload() // sw注册后，会在下次访问时才工作，所以这里调用reload()刷新一次页面
               }
             }, 100)
